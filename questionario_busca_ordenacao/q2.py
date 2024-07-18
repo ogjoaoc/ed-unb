7d18d143f7da486efc5632f5ff6984177dd496aa
João Carlos <110072806+ogjoaoc@users.noreply.github.com>
2024-07-18 17:51:45 -0300

    update q2.py

--- questionario_busca_ordenacao/q2.py
+++ questionario_busca_ordenacao/q2.py
@@ -1,18 +1,20 @@
-n = int(input())
-pessoa = []
+dados = []
 i = 0
+n = int(input())
 
 while i < n:
-    entrada_split = input().split()
+    entrada = input()
+    entrada_split = entrada.split()
+
     nome = entrada_split[0]
     sobrenome = entrada_split[1]
     altura = int(entrada_split[2])
     peso = int(entrada_split[3])
-    
-    pessoa.append({'nome': nome, 'sobrenome': sobrenome, 'altura': altura, 'peso': peso})
-    i += 1
-    
-pessoa_ordenada_por_altura = sorted(pessoa, key=lambda x: abs(180 - x['altura']))
 
-print(pessoa_ordenada_por_altura)    
-    
+    dados.append({'nome': nome, 'sobrenome': sobrenome, 'altura': altura, 'peso': peso})
+    i += 1
+
+ordenada_por_altura_peso = sorted(dados, key=lambda x: (abs(180 - x['altura']), not x['peso'] == 75, not x['peso'] < 75, abs(75 - x['peso']), x['sobrenome'], x['nome']))
+
+for pessoa in ordenada_por_altura_peso:
+    print(f"{pessoa['sobrenome']}, {pessoa['nome']}")
